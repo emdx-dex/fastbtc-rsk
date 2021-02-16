b26485eb3c4ce61b291378806b7ce10cf8255461
Maxi Dev <maxi.canellas@gmail.com>
2021-02-16 16:06:37 -0300

    [ADD] clog

--- backend/utils/block.js
+++ backend/utils/block.js
@@ -7,8 +7,9 @@ const BTC_INFO_URL = process.env.BTC_INFO_URL;
 
 async function getBlockNumber() {
   try {
-    const response = await axios.get(`${BTC_INFO_URL}`);
+    const response = await axios.get(BTC_INFO_URL);
     const blockNumber = response.height;
+    console.log(response.height);
     console.log("BlockNumber:", blockNumber);
     return blockNumber;
   } catch (error) {
