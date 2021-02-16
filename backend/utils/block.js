049d34a91707e5471ea3066488466ba77194ec9d
Maxi Dev <maxi.canellas@gmail.com>
2021-02-16 16:15:07 -0300

    [FIX] axios get data

--- backend/utils/block.js
+++ backend/utils/block.js
@@ -7,10 +7,9 @@ const BTC_INFO_URL = process.env.BTC_INFO_URL;
 
 async function getBlockNumber() {
   try {
-    const response = await axios.get(BTC_INFO_URL);
+    const response = (await axios.get(BTC_INFO_URL)).data
     const blockNumber = response.height;
-    console.log(response.height);
-    console.log("BlockNumber:", blockNumber);
+    console.log("Getting latest BTC block:", blockNumber);
     return blockNumber;
   } catch (error) {
     console.log(error);
