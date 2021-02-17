b2db698e00dc145e07f05a497dd0f42292aa1d65
Maxi Dev <maxi.canellas@gmail.com>
2021-02-17 18:10:20 -0300

    [ADD] cleanUp, multisig/signature status, handlers

--- backend/utils/block.js
+++ backend/utils/block.js
@@ -1,10 +1,35 @@
 const _ = require('lodash');
 const axios = require('axios');
+const ElectrumClient = require('@codewarriorr/electrum-client-js');
 
 require('dotenv').config();
 
 const BTC_INFO_URL = process.env.BTC_INFO_URL;
 
+
+async function connect() {
+  try {
+
+    const client = new ElectrumClient(
+      "tn.not.fyi",
+      55002,
+      "ssl"
+    );
+
+    await client.connect();
+
+    console.log('------------------------------------------------');
+
+    return client;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+/**
+ * Get latest block by querying some API service. 
+ */
 async function getBlockNumber() {
   try {
     const response = await axios.get(`${BTC_INFO_URL}/blocks?limit=1`);
@@ -12,11 +37,32 @@ async function getBlockNumber() {
 
     return blockNumber;
   } catch (error) {
-    return 0;
+    return -1;
+  }
+}
+/**
+ * Get the latest block by subscribing to electrum blockheaders and then closing the client
+ * TODO: optimize it.
+ * If return -1 then error happened.
+ */
+async function getLatestBlockElectrumX() {
+  try {
+
+    let client = await connect();
+    let { height } = await client.blockchain_headers_subscribe();
+
+    await client.close();
+
+    return height;
+
+  } catch (error) {
+    console.log(error);
+    return -1;
   }
 }
 
 module.exports = {
-  getBlockNumber
+  getBlockNumber: getBlockNumber,
+  getLatestBlockElectrumX: getLatestBlockElectrumX
 };
 
