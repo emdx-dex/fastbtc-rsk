69fe7382c85689da4733405a7e5d15d1210957ad
Maxi Dev <maxi.canellas@gmail.com>
2021-02-18 13:22:43 -0300

    [ADD] fallback in case of Electrum getblockX failing

--- backend/utils/block.js
+++ backend/utils/block.js
@@ -7,19 +7,20 @@ require('dotenv').config();
 const BTC_ELECTRUM_PORT = process.env.BTC_ELECTRUM_PORT;
 const BTC_ELECTRUM_PROTOCOL = process.env.BTC_ELECTRUM_PROTOCOL;
 const BTC_ELECTRUM_URI = process.env.BTC_ELECTRUM_URI;
+const BTC_INFO_URL = process.env.BTC_INFO_URL;
+
 let client;
 
 async function connect() {
   try {
     if (_.isEmpty(client)) {
       client = new ElectrumClient(
-        BTC_ELECTRUM_URI,
-        BTC_ELECTRUM_PORT,
-        BTC_ELECTRUM_PROTOCOL
+        process.env.BTC_ELECTRUM_URI,
+        process.env.BTC_ELECTRUM_PORT,
+        process.env.BTC_ELECTRUM_PROTOCOL
       );
       await client.connect();
     }
-
     return client;
   } catch (error) {
     console.log(error);
@@ -28,15 +29,15 @@ async function connect() {
 };
 
 /**
- * Get the latest block by subscribing to electrum blockheaders and then closing the client
- * If return -1 then error happened.
+ * Get latest block by querying some API service. 
  */
-async function getBlockNumber() {
+async function getBlockNumberApi() {
   try {
-    const client = await connect();
-    const { height } = await client.blockchain_headers_subscribe();
     
-    return height;
+    const response = await axios.get(`${BTC_INFO_URL}/blocks?limit=1`);
+    const blockNumber = _.get(response, 'data.data[0].id', {});
+    
+    return blockNumber;
   } catch (error) {
     console.log(error);
 
@@ -44,7 +45,47 @@ async function getBlockNumber() {
   }
 }
 
+/**
+ * Get the latest block by subscribing to electrum blockheaders and then closing the client
+ * If return -1 then error happened.
+ */
+async function getBlockNumberElectrumX() {
+  try {
+    const client = await connect();
+    const { height } = await client.blockchain_headers_subscribe();
+    return height;
+  } catch (error) {
+    console.log(error);
+    await client.close();
+    return -1;
+  }
+}
+
+/**
+ * Intento fetchear el latestBlock de Electrum primero.
+ * Si fallo intento de la api de Blockchair
+ * Sino devuelvo -1 y alerto.
+ */
+async function getBlockNumber(){
+
+  let latestBlock;
+
+  latestBlock = await getBlockNumberElectrumX();
+
+  if(latestBlock != -1)
+    return latestBlock;
+
+  //Alert()
+  latestBlock = await getBlockNumberApi();
+
+  if(latestBlock != -1)
+    return latestBlock;
+  
+  //Alert()
+  return -1;
+
+}
+
 module.exports = {
   getBlockNumber: getBlockNumber
 };
-
