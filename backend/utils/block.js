042a18c81c16fcb61d76f308f13deb6b895832e3
afiorenza <agustinfiorenza@gmail.com>
2021-02-18 09:12:13 +0100

    Optimize get latest block with electrum

--- backend/utils/block.js
+++ backend/utils/block.js
@@ -4,65 +4,48 @@ const ElectrumClient = require('@codewarriorr/electrum-client-js');
 
 require('dotenv').config();
 
-const BTC_INFO_URL = process.env.BTC_INFO_URL;
-
+const BTC_ELECTRUM_PORT = process.env.BTC_ELECTRUM_PORT;
+const BTC_ELECTRUM_PROTOCOL = process.env.BTC_ELECTRUM_PROTOCOL;
+const BTC_ELECTRUM_URI = process.env.BTC_ELECTRUM_URI;
+let client;
 
 async function connect() {
   try {
-
-    const client = new ElectrumClient(
-      "tn.not.fyi",
-      55002,
-      "ssl"
-    );
-
-    await client.connect();
-
-    console.log('------------------------------------------------');
+    if (_.isEmpty(client)) {
+      client = new ElectrumClient(
+        BTC_ELECTRUM_URI,
+        BTC_ELECTRUM_PORT,
+        BTC_ELECTRUM_PROTOCOL
+      );
+      await client.connect();
+    }
 
     return client;
   } catch (error) {
     console.log(error);
+
     return error;
   }
 };
 
 /**
- * Get latest block by querying some API service. 
+ * Get the latest block by subscribing to electrum blockheaders and then closing the client
+ * If return -1 then error happened.
  */
 async function getBlockNumber() {
   try {
-    const response = await axios.get(`${BTC_INFO_URL}/blocks?limit=1`);
-    const blockNumber = _.get(response, 'data.data[0].id', {});
-
-    return blockNumber;
-  } catch (error) {
-    return -1;
-  }
-}
-/**
- * Get the latest block by subscribing to electrum blockheaders and then closing the client
- * TODO: optimize it.
- * If return -1 then error happened.
- */
-async function getLatestBlockElectrumX() {
-  try {
-
-    let client = await connect();
-    let { height } = await client.blockchain_headers_subscribe();
-
-    await client.close();
+    const client = await connect();
+    const { height } = await client.blockchain_headers_subscribe();
 
     return height;
-
   } catch (error) {
     console.log(error);
+
     return -1;
   }
 }
 
 module.exports = {
-  getBlockNumber: getBlockNumber,
-  getLatestBlockElectrumX: getLatestBlockElectrumX
+  getBlockNumber
 };
 
