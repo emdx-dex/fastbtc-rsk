c9379124d0bca82f835725cd9eb90ad79a2a8610
Maxi Dev <maxi.canellas@gmail.com>
2021-02-22 19:11:09 -0300

    [FIX] socks query from fe

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,32 +1,21 @@
 module.exports = {
   devServer: {
     //host: '0.0.0.0',
-    port: 5556, // CHANGE YOUR PORT HERE!
     //disableHostCheck: true,
+    port: 5556,
     compress: true,
     public: 'fastbtc.emdx.io', // That solved it
-    //https: false,
-    //hotOnly: false,
+    proxy: {
+      '^/sockjs-node': {
+        target: process.env.VUE_APP_BACKEND_URL,
+        ws: true,
+        changeOrigin: true
+      }
+    },
   },
   transpileDependencies: [
     'vuetify'
   ],
   runtimeCompiler: true,
   outputDir: "../backend/dist"
-  /**
-   *   devServer: {
-    proxy: {
-      '^/sockjs-node': {
-        target: process.env.VUE_APP_BACKEND_URL,
-        ws: true,
-        changeOrigin: true
-      },
-      '^/api': {
-        target: process.env.VUE_APP_BACKEND_URL,
-        ws: true,
-        changeOrigin: true
-      },
-    },
-  },
-   */
 }
