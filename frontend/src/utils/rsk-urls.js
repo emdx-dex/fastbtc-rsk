d0e402b795b357ab9a72b9d5dc4e46848d08d7d6
afiorenza <agustinfiorenza@gmail.com>
2021-02-22 23:03:39 +0100

    Fix link to rsk address

--- frontend/src/utils/rsk-urls.js
+++ frontend/src/utils/rsk-urls.js
@@ -1,7 +1,7 @@
 const RSK_EXPLORER_URL = process.env.VUE_APP_RSK_EXPLORER_URL
 
 export const getRSKAddressUrl = (address) => {
-  return `${RSK_EXPLORER_URL}/address/${address}/`;
+  return `${RSK_EXPLORER_URL}/address/${address.toLowerCase()}/`;
 } 
 
 export const getRSKTxUrl = (tx) => {
